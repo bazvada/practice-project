604fe2008cceae8faf7d76aecbad36db300a13b4
bazvada <bazdyrevadara0@gmail.com>
2026-07-08 16:46:48 +0300

    Инициализация проекта

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "practice-project" SUPERUSER;
+CREATE DATABASE "practice-project" OWNER "practice-project" ENCODING 'utf-8';
